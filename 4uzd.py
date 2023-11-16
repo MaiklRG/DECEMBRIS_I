426000933a12008688d4bc4f371199a1da2a6273
MaiklRG <maikls.reinsons-guza@grobinasskola.lv>
2023-11-16 09:55:59 +0200

    init commit

--- 4uzd.py
+++ 4uzd.py
@@ -1,1 +1,8 @@
 # Izveidot Python programmu, kas aprēķina lietotāja ievadīta skaitļa faktoriālu, izmantojot for ciklu.
+
+a=int(input("Ievadit skaitli seit:"))
+b=0
+for i in range(1,a+1):
+    b=b+i
+
+print(b)
