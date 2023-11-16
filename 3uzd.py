248419105bac1d3e76e67625a7d9afeb9dbb3275
MaiklRG <maikls.reinsons-guza@grobinasskola.lv>
2023-11-16 10:28:39 +0200

    init commit

--- 3uzd.py
+++ 3uzd.py
@@ -1,3 +1,7 @@
 # Izveidot Python programmu, kas pārbauda, vai ievadītais skaitlis ir nepāra, izmantojot if priekšrakstu.
 a=int(input("Ievadi skaitli:"))
-print(bool(a))
+#print(bool(a))
+nepara_skaitlis=[]
+for skaitlis in a:
+    if skaitlis%2!=0:
+        print(nepara_skaitlis)
